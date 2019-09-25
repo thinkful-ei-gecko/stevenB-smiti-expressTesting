8da79c476a9fa0fc051d157230adfa9d143a3ca5
Steven Bull <stevenwbulljr@gmail.com>
2019-09-25 11:10:19 -0500

    add server.js file to handle server listening

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require('express');
 const morgan = require('morgan');
 
 const app = express();
-const PORT = 8000;
 
 app.use(morgan('dev'));
 
@@ -10,4 +9,4 @@ app.get('/', ( req, res ) => {
   res.send('Hello Express!');
 });
 
-app.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`));
+module.exports = app;
---
+++ server.js
@@ -0,0 +1,4 @@
+const app = require('./app');
+
+const PORT = 8000;
+app.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`));
