d43ecdaec55a0f05afe7a59289b8a9a1f4d6d16f
Steven Bull <stevenwbulljr@gmail.com>
2019-09-25 11:38:08 -0500

    finish /generate endpoint testing

--- test/app.test.js
+++ test/app.test.js
@@ -25,3 +25,18 @@ describe('GET /sum', () => {
       .expect(400, 'Value for a is needed');
   });
 });
+
+describe('GET /generate endpoint', () => {
+  it('should generate an array of 5', () => {
+    return supertest(app)
+      .get('/generate')
+      .query({ n: 5})
+      .expect(200)
+      .expect('Content-Type', /json/)
+      .then( res => {
+        expect(res.body).to.be.an('array');
+        expect(res.body).to.have.lengthOf.at.least(1);
+        expect(res.body).to.include.members([1, 2, 3, 4, 5]);
+      });
+  });
+});
