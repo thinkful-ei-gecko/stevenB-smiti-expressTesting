f5dbe979d695879716243ccee194015a2f644493
Steven Bull <stevenwbulljr@gmail.com>
2019-09-25 11:29:14 -0500

    add /generate pathway logic

--- app.js
+++ app.js
@@ -51,4 +51,33 @@ app.get('/sum', (req, res) => {
     .send(`${a} divided by ${b} is ${ans}`);
 });
 
+app.get('/generate', (req, res) => {
+  // get n from the query string in the request
+  const { n } = req.query;
+
+  // coerce n to a numeric value
+  const num = parseInt(n);
+
+  if (Number.isNaN(num)) {
+    return res
+      .status(400)
+      .send('Invalid request');
+  }
+
+  // generate array [1..n]
+  const initial =  Array(num)
+    .fill(1)
+    .map((_, i) => i + 1);
+
+  // shuffle the array
+  initial.forEach((e, i) => {
+    let ran = Math.floor(Math.random() * num);
+    let temp = initial[i];
+    initial[i] = initial[ran];
+    initial[ran] = temp;
+  });
+
+  res.json(initial);
+});
+
 module.exports = app;
